112bb701e8441ca1d556d96ecadb9f7fbaf45039
유세영\usaey <usaeyoung9514@gmail.com>
2025-06-08 16:21:57 +0900

    초기 커밋.

---
+++ CacheTest/LocalStorage.cs
@@ -0,0 +1,44 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace CacheTest
+{
+    public class LocalStorage
+    {
+        private const string CultureKey = "culture";
+
+        private readonly ILocalStorageService _localStorageService;
+
+        public string Culture { get; set; }
+
+        public LocalStorage(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+
+        public async Task InitializeCultureAsync()
+        {
+            var culture = await _localStorageService.GetItemAsync<string>(CultureKey);
+            if (culture != null)
+            {
+                Culture = culture;
+            }
+            else
+            {
+                Culture = "ko-KR";
+                await SaveCultureAsync(Culture);
+            }
+        }
+
+        public async Task SaveCultureAsync(string culture)
+        {
+            await _localStorageService.SetItemAsync(CultureKey, culture);
+        }
+
+        public async Task<string?> GetCultureAsync()
+        {
+            return await _localStorageService.GetItemAsync<string?>(CultureKey);
+        }
+    }
+}
---
+++ CacheTest/Program.cs
@@ -0,0 +1,19 @@
+using CacheTest;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using Blazored.LocalStorage;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+
+// Local Storage 를 위한 정의들
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<LocalStorage>();
+// 끝
+
+await builder.Build().RunAsync();
